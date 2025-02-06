8650479e2f483df84be65a56ed530261281774c7
unknown <muhammedashique8281@gmail.com>
2025-02-06 18:51:11 +0530

    Changes Updated

--- yasviproject/settings.py
+++ yasviproject/settings.py
@@ -15,7 +15,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://yasvitravels.com',
+    
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,8 +29,7 @@ SECRET_KEY = 'django-insecure-r%^jrk_77&uzyxyb55dqu-%7dxwzn8h2^hzw$a7dul%pv^5fwj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['yasvitravels.com','www.yasvitravels.com','127.0.0.1','localhost']
 
 # Application definition
 
